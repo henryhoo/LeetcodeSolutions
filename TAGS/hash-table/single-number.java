d4f8b495c7c1a3f39353712a2c631a586101eb3b
henryhoo <497018787@qq.com>
2016-09-08 10:49:22 -0400

    add 136

--- TAGS/hash-table/single-number.java
+++ TAGS/hash-table/single-number.java
@@ -8,8 +8,4 @@ Number  (H) Find the Duplicate Number
 */
 
 
-public class Solution {
-    public int singleNumber(int[] nums) {
-        
-    }
-}
+public class Solution {    public int singleNumber(int[] nums) {        int res = nums[0];        for (int i = 1; i < nums.length; i++) {            res = res ^ nums[i];        }        return res;    }}
