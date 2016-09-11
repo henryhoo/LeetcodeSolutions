7588cb4c7615c48ebfd24bc8c796570340e21033
henryhoo <497018787@qq.com>
2016-09-11 15:49:30 -0400

    add 2

--- TAGS/math/add-two-numbers.java
+++ TAGS/math/add-two-numbers.java
@@ -16,8 +16,35 @@ Multiply Strings  (E) Add Binary
  *     ListNode(int x) { val = x; }
  * }
  */
-public class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
-    }
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode walk = dummy;
+        int sum = 0;
+        while (l1 != null || l2 != null) {
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            walk.next = new ListNode (sum % 10);
+            walk = walk.next;
+            sum = (sum/10) > 0 ? 1 : 0;
+        }
+        if (sum == 1) {
+             walk.next = new ListNode (1);
+        }
+        return dummy.next;
+    }
 }
