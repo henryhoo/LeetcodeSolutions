214ee895930fc61528a28ec101145a716713c17d
henryhoo <497018787@qq.com>
2016-09-11 23:18:16 -0400

    add 151

--- TAGS/string/reverse-words-in-a-string.java
+++ TAGS/string/reverse-words-in-a-string.java
@@ -12,8 +12,4 @@ Reverse Words in a String II
 */
 
 
-public class Solution {
-    public String reverseWords(String s) {
-        
-    }
-}
+public class Solution {    public String reverseWords(String s) {        String[] temp = s.trim().split(" +");        StringBuilder sb = new StringBuilder();        for (int i = temp.length -1; i >= 0; i--) {            sb.append(temp[i] + " ");        }        return sb.toString().trim();    }}
