ce405ff42938d755ea023fe65aa0721c7c024846
henryhoo <497018787@qq.com>
2016-09-18 23:22:53 -0400

    20160918-

--- TAGS/tree/lowest-common-ancestor-of-a-binary-tree.java
+++ TAGS/tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -17,17 +17,4 @@ Common Ancestor of a Binary Search Tree
 */
 
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-public class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        
-    }
-}
+/** * Definition for a binary tree node. * public class TreeNode { *     int val; *     TreeNode left; *     TreeNode right; *     TreeNode(int x) { val = x; } * } */public class Solution {    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {        if (root == null || root == p || root == q) return root;        TreeNode left = lowestCommonAncestor(root.left, p , q);        TreeNode right = lowestCommonAncestor(root.right, p , q);        if (left != null && right != null) return root;        return left != null ? left : right;    }}
