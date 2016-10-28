95161b9ac41bf1bdb5bf16cfd06dd2ee24d99bc1
henryhoo <497018787@qq.com>
2016-10-28 16:45:03 -0400

    20161028+

--- TAGS/string/multiply-strings.java
+++ TAGS/string/multiply-strings.java
@@ -7,9 +7,23 @@ this question    Show Tags   Math  String     Show Similar Problems   (M) Add
 Two Numbers  (E) Plus One  (E) Add Binary
 */
 
-
-public class Solution {
-    public String multiply(String num1, String num2) {
-        
-    }
+public class Solution {
+    public String multiply(String num1, String num2) {
+        int[] res = new int[num1.length() + num2.length()];
+        for (int i = num2.length() - 1; i>=0; i--) {
+            for (int j = num1.length()-1; j>=0; j--) {
+                int mul = (num1.charAt(j) - '0') * (num2.charAt(i) - '0');
+                int sum = mul + res[i+j+1];
+                res[i+j+1] = sum%10;
+                res[i+j] += sum/10;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (res[i] == 0 && i<res.length-1) i++;
+        for (i = i; i<res.length; i++) {
+            sb.append(res[i]);
+        }
+        return sb.toString();
+    }
 }
