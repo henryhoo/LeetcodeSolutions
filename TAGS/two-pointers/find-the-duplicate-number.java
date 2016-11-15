81b41cd2486d507e03c9ca0321f835caa4fa4a44
henryhoo <497018787@qq.com>
2016-11-15 00:21:43 -0500

    20161114-

--- TAGS/two-pointers/find-the-duplicate-number.java
+++ TAGS/two-pointers/find-the-duplicate-number.java
@@ -13,8 +13,4 @@ List Cycle II  (M) Missing Number
 */
 
 
-public class Solution {
-    public int findDuplicate(int[] nums) {
-        
-    }
-}
+public class Solution {    public int findDuplicate(int[] nums) {        int slow = 0, fast = 0;        do{            slow = nums[slow];            fast = nums[nums[fast]];        }while(slow != fast);        slow = 0;        while(slow != fast){            slow = nums[slow];            fast = nums[fast];        }        return slow;    }}
