d9008c1b667ea4c95644e7e582620cffbb153a82
henryhoo <497018787@qq.com>
2016-11-17 20:59:43 -0500

    20161117-

--- TAGS/string/length-of-last-word.java
+++ TAGS/string/length-of-last-word.java
@@ -8,8 +8,4 @@ String
 */
 
 
-public class Solution {
-    public int lengthOfLastWord(String s) {
-        
-    }
-}
+public class Solution {    public int lengthOfLastWord(String s) {        String[] ss = s.trim().split(" ");        return ss[ss.length - 1].length();    }}public int lengthOfLastWord(String s) {    return s.trim().length()-s.trim().lastIndexOf(" ")-1;}
