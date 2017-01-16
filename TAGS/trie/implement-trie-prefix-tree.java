85e799ef281eda65ae871ff799cbfc13ad100334
henryhoo <497018787@qq.com>
2017-01-15 23:05:57 -0500

    update solutions

--- TAGS/trie/implement-trie-prefix-tree.java
+++ TAGS/trie/implement-trie-prefix-tree.java
@@ -6,36 +6,7 @@ Problems   (M) Add and Search Word - Data structure design
 */
 
 
-class TrieNode {
-    // Initialize your data structure here.
-    public TrieNode() {
-        
-    }
-}
-
-public class Trie {
-    private TrieNode root;
-
-    public Trie() {
-        root = new TrieNode();
-    }
-
-    // Inserts a word into the trie.
-    public void insert(String word) {
-        
-    }
-
-    // Returns if the word is in the trie.
-    public boolean search(String word) {
-        
-    }
-
-    // Returns if there is any word in the trie
-    // that starts with the given prefix.
-    public boolean startsWith(String prefix) {
-        
-    }
-}
+class TrieNode {    // Initialize your data structure here.    public boolean isWord;    public TrieNode[] next = new TrieNode[26];    public char val;    public TrieNode() {    }    public TrieNode(char c) {        TrieNode node = new TrieNode();        node.val = c;    }}public class Trie {    private TrieNode root;    public Trie() {        root = new TrieNode();        root.val = ' ';    }    // Inserts a word into the trie.    public void insert(String word) {        TrieNode walk = root;        for (int i = 0; i < word.length(); i++) {            char c = word.charAt(i);            if (walk.next[c - 'a'] == null)                walk.next[c - 'a'] = new TrieNode(c);            walk = walk.next[c - 'a'];        }        walk.isWord = true;    }    // Returns if the word is in the trie.    public boolean search(String word) {        TrieNode walk = root;        for (int i = 0; i < word.length(); i++) {            char c = word.charAt(i);            if (walk.next[c - 'a'] == null)                return false;            walk = walk.next[c - 'a'];        }        return walk.isWord;    }    // Returns if there is any word in the trie    // that starts with the given prefix.    public boolean startsWith(String prefix) {        TrieNode walk = root;        for (int i = 0; i < prefix.length(); i++) {            char c = prefix.charAt(i);            if (walk.next[c - 'a'] == null)                return false;            walk = walk.next[c - 'a'];        }        return true;    }}// Your Trie object will be instantiated and called as such:// Trie trie = new Trie();// trie.insert("somestring");// trie.search("key");
 
 // Your Trie object will be instantiated and called as such:
 // Trie trie = new Trie();
